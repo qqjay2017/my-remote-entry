f195ab0fb6fe743a34be6ac3d984156f6890fcbf
huangbo <huangbo@lj-ch.com>
2022-10-20 22:28:40 +0800

    feat: 引入craco11111

---
+++ craco.config.js
@@ -0,0 +1,39 @@
+const { resolve } = require("path");
+const CracoLessPlugin = require("craco-less");
+const cracoModuleFederation = require("craco-module-federation");
+const { name } = require("./package");
+const env = process.env.NODE_ENV || "production";
+const isProd = env === "production";
+const isSingle = process.env.REACT_APP_MODE == "single";
+/** @type {import('@craco/craco').CracoConfig} */
+const config = {
+  plugins: [
+    {
+      plugin: cracoModuleFederation,
+    },
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              primary: "#1677FF",
+              "primary-color": "#1677FF",
+              "text-color": "rgba(0, 0, 0, 0.65)",
+              "table-header-color": "rgba(0, 0, 0, 0.85)",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+
+  webpack: {},
+
+  devServer: {
+    open: false,
+  },
+};
+
+module.exports = config;
