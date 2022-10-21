389ff2a22ff4d146af09e62719a727f2818f8759
huangbo <huangbo@lj-ch.com>
2022-10-21 08:05:00 +0800

    feat: 测试共享依赖11111

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import * as antd from "antd";
+import dayjs from "dayjs";
+import _ from "lodash-es";
+import ahooks from "ahooks";
+import * as procomponents from "@ant-design/pro-components";
+import * as reactRouterDom from "react-router-dom";
+
+console.log(antd, dayjs, _, ahooks, procomponents, reactRouterDom);
 
 ReactDOM.render(
   <React.StrictMode>
